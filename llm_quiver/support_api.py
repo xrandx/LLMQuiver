48aebfa8c90f528a4a6c2a72e3db81a06bb9da47
xrandx <kevtyle@hotmail.com>
2024-10-12 16:07:47 +0800

    remove redundant codes.

--- llm_quiver/support_api.py
+++ llm_quiver/support_api.py
@@ -6,10 +6,3 @@ class SupportAPI(Enum):
     AzureOpenAI = "azure_openai"
     OpenAI = "openai"
     OpenAILike = "openai_like"
-
-
-api_type = "azure_openai"
-try:
-    print(SupportAPI(api_type))
-except KeyError:
-    raise ValueError(f"{api_type} is not a valid name for {SupportAPI.__name__}")
